dbf5230e529215dc9b1fb50fa6a4e2c0cdffb1ca
SeanConway102 <srconway0@gmail.com>
2018-03-19 09:57:10 -0400

    Made changes

--- hello.java
+++ hello.java
@@ -1,15 +1,17 @@
 // a simple program to practice common Github commands 
+import java.util.Scanner
 
 public class hello {
 
     public static void main(String [] args) {
 
-
+	Scanner input = new Scanner(System.in)
 	
 	// added  line	
 	// added another line
-
-        System.out.println("Hello Class!");
+	System.out.println("Please enter your name");
+	name = input.nextLine()
+        System.out.println("Hello"+name+"!");
 
 
 
